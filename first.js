6322ad0023992c07891e8136172541e0c4901b3c
Ashishkumar667 <ashishkumarinfo6@gmail.com>
2024-06-19 16:37:21 +0530

    first.js

--- first.js
+++ first.js
@@ -1,18 +1,24 @@
-// const btn= document.getElementsByClassName("talk")
-// const content=document.getElementsByClassName("content")
-// or we can do this by another method also.
+// // const btn= document.getElementsByClassName("talk")
+// // const content=document.getElementsByClassName("content")
+// // or we can do this by another method also.
 
-// const btn= document.querySelector('talk') (for class)
-//  const btn= document.querySelector('#talk') (for Id)
+// // const btn= document.querySelector('talk') (for class)
+// //  const btn= document.querySelector('#talk') (for Id)
 const btn = document.querySelector('.talk')
 const content= document.querySelector('.content')
+
+function gotolink(link){
+console.log(link.value)
+location.href='https://forms.gle/M3oKv6Wd7Zc6VLfF9', "_blank";
+};
+
 
 
 function speak(text){
     const text_speak = new SpeechSynthesisUtterance(text)
     text_speak.rate =1;
     text_speak.volume = 1;
-    text_speak.pitch =1;
+    text_speak.pitch =2;
 
     window.speechSynthesis.speak(text_speak);
 }
@@ -24,12 +30,12 @@ function wishMe(){
         speak("Good Morning Sir...")
     }
 
-    else if( hour >12 && hour<17){
-        speak("Good Aternoon Sir...")
+    else if( hour>17 ){
+        speak("Good Evening sir...")
     }
 
     else{
-        speak("Good Evening Sir ...")
+        speak("Good Afternoon sir ...")
     }
 }
 
@@ -60,9 +66,7 @@ function takeCommand(message){
     if (message.includes('hey')||message.includes('hello')){
         speak('hello sir, How may I help you?')
     }
-    else{
-        speak("Please, Make it more clear...")
-    }
+    
     if (message.includes('open Google')){
         window.open("https://google.com","_blank");
         speak("Opening google")
@@ -83,17 +87,18 @@ function takeCommand(message){
         window.open("https://Cardekho.com","_blank");
         speak("Opening CarDekho")
     }
+    
 
    else if(message.includes('What is') || message.includes('who is') || message.includes('What are')){
     window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
-    const finalvoiceofcalvin="This is what I found on the intrnet for you" + message;
+    const finalvoiceofcalvin ="This is what I found on the intrnet for you" + message;
     speak(finalvoiceofcalvin);
    }
 
 
    else if(message.includes('wikipedia')){
     window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
-    const finalvoiceofcalvin="This is what I found on the intrnet for you" + message;
+    const finalvoiceofcalvin="This is what I found on the intrnet for " + message;
     speak(finalvoiceofcalvin);
    }
 
@@ -104,7 +109,7 @@ function takeCommand(message){
    }
 
    else if(message.includes('date')){
-    const date= new Date().toLocaleString(undefined,{month: "short", day: "numeric"});
+    const date= new Date().toLocaleString(undefined,{month: "short", day: "numeric", year:"numeric"});
     const finalvoiceofcalvin = date;
     speak(finalvoiceofcalvin);
    }
@@ -115,9 +120,28 @@ function takeCommand(message){
     speak(finalvoiceofcalvin);
   }
 
-  else{
-    window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank")
-    const finalvoiceofcalvin ="Information that I found For your message" + message + "On google";
+  else if (message.includes('calender')){
+    window.open('calender:///')
+    const finalvoiceofcalvin ="Opening calender"
     speak(finalvoiceofcalvin);
   }
+else if(message.includes('calvin who made you?')) {
+    speak('Ashish mishra has made me');
+}
+
+ else if(message.include('calvin, tell me a joke in hindi')){
+   speak(' एक चुटकुला आ रहा है! समुद्री राक्षस का पसंदीदा नाश्ता क्या है?' + 'उत्तर है जहाज़ और डुबकी hehehe..')
+ }
+ else if(message.include('calvin, tell me a joke')){
+    speak(' What falls, but never needs a bandage?'+' answer is The rain.');
+  }
+else if(message.include('Calvin,Where do you live')){
+    speak('I live in your heart');
+}
+  else{
+    window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank")
+    const finalvoiceofcalvin ="Information that I found For " + message + "On google";
+    speak(finalvoiceofcalvin);
+  }
+  
 }
